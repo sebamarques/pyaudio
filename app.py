08cb3dceaffd0247a786bdeb6f1270b777ba9363
Sebastián Márquez <89924524+sebamarques@users.noreply.github.com>
2022-12-05 18:30:44 -0300

    0.1.0

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
 import pyaudio
 import wave
 import whisper
+import tkinter
+valor = 0
+def grabar():
+    print("grabando")
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100,input=True,frames_per_buffer=1024)
+    frames= []
+    
+    while True:
+        data = stream.read(1024,exception_on_overflow=False)
+        frames.append(data)
+        if valor == 10:
+            print("hola")
+            break
+            
 
-while True:
-    print("a = grabar audio\n b= transcribir audio\nc=terminar programa")
-    x = input("")
-    if x == "a":
-        audio = pyaudio.PyAudio()
-        stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100,input=True,frames_per_buffer=1024)
-        frames= []
-        try:
-            while True:
-                data = stream.read(1024,exception_on_overflow=False)
-                frames.append(data)
-        except KeyboardInterrupt:
-            pass
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
+    sound_file = wave.open("myrecording.wav","wb")
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
 
-        sound_file = wave.open("myrecording.wav","wb")
-        sound_file.setnchannels(1)
-        sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-        sound_file.setframerate(44100)
-        sound_file.writeframes(b''.join(frames))
-        sound_file.close()
-    if x == "b": 
+def reproducir():
+        print("reproduciendo")
         model = whisper.load_model("base")
         result = model.transcribe('myrecording.wav')
         print(result["text"])    
         with open("nuevo.txt","a") as texto:
             texto.write(result["text"])
-    elif x == "c":
-        break
-        
+def salir():
+    ventana.destroy()
+
+def parar():
+    valor = 10
+    print(valor)
+    return valor
+ventana = tkinter.Tk()
+ventana.geometry("400x300")
+boton = tkinter.Button(ventana, text="presionar",command=grabar)
+boton.pack()
+boton1 = tkinter.Button(ventana,bg="blue",text="reproducir audio",command=reproducir)
+boton1.pack()
+boton3 = tkinter.Button(ventana,text="salir",command=salir)
+boton3.pack()
+boton4 = tkinter.Button(ventana,text="parar",command=parar())
+boton4.pack()
+ventana.mainloop()
